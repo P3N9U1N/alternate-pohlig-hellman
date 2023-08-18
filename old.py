4e0bea7a4267d15719055e218414d2d07cccb591
Thomas Heizmann <ThomasHeizmann@hotmail.com>
2023-08-18 17:32:55 +0200

    Add experimental space-time tradeoff

--- old.py
+++ old.py
@@ -1,3 +1,6 @@
+"""
+Old version of the algorithm without Babystep-Giantstep space–time tradeoff
+"""
 import labmath
 
 class DiscreteLogarithm:
@@ -24,57 +27,32 @@ class DiscreteLogarithm:
         self.__mod=mod        
         self.__order=labmath.multord(base,mod)
         self.__factors=list(labmath.primefac(self.__order))
-        
-        
-
-        #for c in range(mod):
-        #    v=pow(base,c,mod)
-        #    print(v)
-
-        d=self.order
-        factors=self.__factors      
-        self.__giant_steps={}
-        giant_steps=self.__giant_steps
-        for f in factors:    
-            d=d//f
-            cache={}
-            giant_steps[d]=cache
-            size=pow(base,d,mod)
-            value=1
-            for i in range(f-1,-1,-1):                
-                value= (value*size) % mod
-                cache[value]=i
-            base=pow(base,f,mod)  
-        pass
-
-
- 
- 
 
     def calc(self,value:int)->int:
         "Calculates discrete logarithm. Returns -1 if no result exists."
         mod=self.mod
         d=self.order
         factors=self.__factors
-        giant_steps=self.__giant_steps
         base=self.base
         steps=0
         step=1       
         for f in factors:    
             d=d//f
-            cache=giant_steps[d]
             start=pow(value,d,mod)    
             if start != 1:
                 size=pow(base,d,mod)
-
-                offset=cache.get(start)
-                if offset is None: return -1 
-
+                offset=0 
+                while offset < f and start != 1:
+                    start=(start*size) % mod            
+                    offset+=1
+                if start !=1: return -1 
                 value=(value*pow(base,offset,mod)) % mod
                 steps=steps+step*offset 
-
             base=pow(base,f,mod)   
             step*=f
         return self.order-steps
+
+
+
 
 
